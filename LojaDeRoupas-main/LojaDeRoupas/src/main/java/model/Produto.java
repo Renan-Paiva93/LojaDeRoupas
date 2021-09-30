c50104b7ad738ea9fc6be47f44c1f9b2c25fce2a
Renan Paiva <71081313+Renan-Paiva93@users.noreply.github.com>
2021-09-29 22:46:35 -0300

    criação das classes da model

---
+++ LojaDeRoupas-main/LojaDeRoupas/src/main/java/model/Produto.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Dell
+ */
+public class Produto {
+    
+    public static int produtosCadastrados;
+    private int id, quantidade;
+    private double valor;
+    private String nome,fornecedor,descricao;
+
+    public Produto() {
+        produtosCadastrados++;
+        this.id = produtosCadastrados;
+    }
+
+    public static int getProdutosCadastrados() {
+        return produtosCadastrados;
+    }
+
+    public static void setProdutosCadastrados(int produtosCadastrados) {
+        Produto.produtosCadastrados = produtosCadastrados;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
